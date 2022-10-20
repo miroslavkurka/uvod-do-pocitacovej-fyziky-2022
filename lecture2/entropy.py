d9d8b0bc7b72ae837078953a53fc0a245dc7d6a9
Miro Kurka <59745102+miroslavkurka@users.noreply.github.com>
2022-10-20 14:33:27 +0200

    Create entropy.py

---
+++ lecture2/entropy.py
@@ -0,0 +1,23 @@
+# distribucia 
+#
+#
+#
+
+
+def calculate_probability(my_text):
+    # get the probability, P(word)= N of word/all words in text
+    text_dict={}
+    with open(my_text) as f:
+        for line in f: 
+            for word in line.split():
+                text_dict[word]+=1 
+    
+    return text_dict
+
+def get_entropy(my_file):
+
+
+    entropy=0 
+
+    return entropy
+
